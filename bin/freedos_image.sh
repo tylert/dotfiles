8d27636a5d1fd3e4f492fd7396a9f75ad01798f4
Tyler Tidman <tyler.tidman@draak.ca>
2014-11-23 16:17:32 -0500

    Push FreeDOS generator script

--- bin/freedos_image.sh
+++ bin/freedos_image.sh
@@ -1,21 +1,26 @@
+#!/usr/bin/env bash
 
 # http://chtaube.eu/computers/freedos/bootable-usb/
 
-# Use a pre-built one:
-
 # dd if=freedos.img of=/dev/sdz bs=512k
-
-
-# Build your own:
 
 file="${1}"
 
-dd if=/dev/zero of=${file} bs=1M count=100
+size_in_mb='96'
+
+dd if=/dev/zero of=${file} bs=512k count=$((2 * ${size_in_mb}))
 
 parted -s ${file} mklabel msdos
-parted -s ${file} mkpart primary fat32 0 100MB
+parted -s ${file} mkpart primary fat32 1 100%
 parted -s ${file} set 1 boot on
 
-# mount -o loop ${file} /mntpoint
-# mkfs.fat -F 32 -n FREEDOS /dev/mapper/loop0
+mkfs.fat -F 32 -n FREEDOS ${file}
 syslinux -i ${file}
+
+mcopy -i ${file} SYSLINUX.CFG ::
+mcopy -i ${file} AUTOEXEC.BAT ::
+mcopy -i ${file} CONFIG.SYS ::
+
+# pushd FREEDOS/PACKAGES/BASE
+# for i in *.ZIP ; do unzip -o -d tmp $i ; done
+# mcopy -i ${file} FREEDOS/PACKAGES/BASE/tmp/{bin,BIN}/* ::
