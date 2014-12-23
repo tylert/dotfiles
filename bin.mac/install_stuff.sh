e41e60c46a0487fdaf18e2e2d6c0f3c85464df2f
Tyler Tidman <tyler.tidman@draak.ca>
2014-12-23 16:02:22 -0500

    Fix up the mac helper scripts a bit

--- bin.mac/install_stuff.sh
+++ bin.mac/install_stuff.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Fetch the latest version of Xcode appropriate for your current OS X release
+# Visit https://developer.apple.com/downloads
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew doctor
+
 brew install macvim
 brew install git
 brew install awscli
+
+sudo easy_install pip
