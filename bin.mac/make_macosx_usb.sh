0e3235768e2978bea0cb7151eb2b9451bb7d4713
Tyler Tidman <tyler.tidman@draak.ca>
2015-12-14 14:26:22 -0500

    Tidy up the dotfiles stuff a bit more

--- bin.mac/make_macosx_usb.sh
+++ bin.mac/make_macosx_usb.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-app_path="${HOME}/install/macosx"
-app_name='Install OS X Yosemite.app'
+app_path='/Applications'
+app_name='Install OS X El Capitan.app'
 
-target='/Volumes/UNTITLED'
+target='/Volumes/foo'
 
 sudo "${app_path}/${app_name}/Contents/Resources/createinstallmedia" \
   --volume "${target}" \
