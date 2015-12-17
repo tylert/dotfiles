e4985310d12df1f70a28f2c65baff1fb675bee0f
Tyler Tidman <tyler.tidman@draak.ca>
2015-12-17 11:44:32 -0500

    Add some more essential mac packages

--- bin.mac/install_stuff.sh
+++ bin.mac/install_stuff.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
-# Fetch the latest version of Xcode appropriate for your current OS X release
-# Visit https://developer.apple.com/downloads
-
-# homebrew
+# Install homebrew (requires Xcode and Xcode command-line tools)
+# https://developer.apple.com/downloads
+# http://brew.sh
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install git
 brew install macvim
+brew install gpg
+brew install rename
+brew install wget
 
-# pip/virtualenv
+# Install pip/virtualenv
 sudo easy_install pip
 sudo -H pip install virtualenv
 
-# rvm
+# Install rvm
+# http://rvm.io
 brew install gpg
 gpg --keyserver hkp://keys.gnupg.net --recv-key 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
-
 #\curl -O https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer
 #\curl -O https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer.asc
 #gpg --verify rvm-installer.asc && bash rvm-installer stable
