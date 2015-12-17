8491eab97c2ef7c57f35e276e4414e266c45ab1d
Tyler Tidman <tyler.tidman@draak.ca>
2015-12-17 09:39:47 -0500

    Pull in some new changes that need backing up

--- bin.mac/install_stuff.sh
+++ bin.mac/install_stuff.sh
@@ -3,12 +3,20 @@
 # Fetch the latest version of Xcode appropriate for your current OS X release
 # Visit https://developer.apple.com/downloads
 
+# homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew doctor
-
-brew install macvim
 brew install git
-brew install awscli
+brew install macvim
 
+# pip/virtualenv
 sudo easy_install pip
+sudo -H pip install virtualenv
+
+# rvm
+brew install gpg
+gpg --keyserver hkp://keys.gnupg.net --recv-key 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+
+#\curl -O https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer
+#\curl -O https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer.asc
+#gpg --verify rvm-installer.asc && bash rvm-installer stable
