cf1a2864ecc3c933863208d466f9e13f99e6c7a9
Tyler Tidman <tyler.tidman@draak.ca>
2017-10-26 17:02:16 -0400

    Reminder comments in gen_mailmap helper script

--- bin/gen_mailmap.sh
+++ bin/gen_mailmap.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-# Create a new stub mailmap file for use in git repos.
+# Create a new stub mailmap file for use in git repos.  Dump the output to
+# .mailmap and edit the file to correct all the left-hand entries.  Cull any
+# entries where left = right and be sure to sort|uniq to remove duplicate
+# lines.  Move .mailmap to a temp location if regenerating and append to it.
 
-git shortlog -se | awk -F'\t' '{print $2,$2}' | sort | uniq
+git shortlog --email --summary | awk -F '\t' '{print $2,$2}' | sort | uniq
