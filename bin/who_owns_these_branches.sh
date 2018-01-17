5305229125ea07d57b7c497cd89a6bce4e5dd5e1
Tyler Tidman <tyler.tidman@draak.ca>
2018-01-17 18:13:13 -0500

    Make sure the who_owns_these_branches script shows merged and not merged branches

--- bin/who_owns_these_branches.sh
+++ bin/who_owns_these_branches.sh
@@ -2,6 +2,14 @@
 
 # Print out a list of branches along with the last committer on each
 
-for branch in $(git branch --remotes | grep --invert-match HEAD); do
+echo '-----===[ NOT MERGED ]===-----'
+
+for branch in $(git branch --remotes --no-merged | grep --invert-match HEAD); do
+    echo -e $(git show --format='%h %aN' ${branch} | head -n 1) \\t${branch}
+done | sort
+
+echo '-----===[ MERGED ]===-----'
+
+for branch in $(git branch --remotes --merged | grep --invert-match HEAD); do
     echo -e $(git show --format='%h %aN' ${branch} | head -n 1) \\t${branch}
 done | sort
