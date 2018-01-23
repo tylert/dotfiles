591801fcae6e389885edb27fc5e7fbd3bce5185a
Tyler Tidman <tyler.tidman@draak.ca>
2018-01-23 17:23:29 -0500

    Make who owns branches script check if in git repo first

--- bin/who_owns_these_branches.sh
+++ bin/who_owns_these_branches.sh
@@ -2,16 +2,22 @@
 
 # Print out a list of branches along with the last committer on each
 
-git branch
+# Don't do anything if we're not inside an existing git repo
+if $(git rev-parse --quiet --git-dir &> /dev/null); then
+    git remote -v
+    git branch
 
-echo '-----===[ NOT MERGED ]===-----'
+    echo '-----===[ NOT MERGED ]===-----'
 
-for branch in $(git branch --remotes --no-merged | grep --invert-match HEAD); do
-    echo -e $(git show --format='%h %aN' ${branch} | head -n 1) \\t${branch}
-done | sort -k2
+    for branch in $(git branch --remotes --no-merged | grep --invert-match HEAD); do
+        echo -e $(git show --format='%h %aN' ${branch} | head -n 1) \\t${branch}
+    done | sort -k2
 
-echo '-----===[ MERGED ]===-----'
+    echo '-----===[ MERGED ]===-----'
 
-for branch in $(git branch --remotes --merged | grep --invert-match HEAD); do
-    echo -e $(git show --format='%h %aN' ${branch} | head -n 1) \\t${branch}
-done | sort -k2
+    for branch in $(git branch --remotes --merged | grep --invert-match HEAD); do
+        echo -e $(git show --format='%h %aN' ${branch} | head -n 1) \\t${branch}
+    done | sort -k2
+else
+    echo 'This is not a git repo'
+fi
