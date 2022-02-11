f5e2d3f34dba129e78ec1791356c38e9e39d7ce1
Tyler Tidman <tyler.tidman@draak.ca>
2022-02-11 11:15:12 -0500

    Add jhead for brew

--- install_on_macos_to_suck_less.sh
+++ install_on_macos_to_suck_less.sh
@@ -7,9 +7,9 @@
 # to create the list below...
 brew_formulae='
 awscli bash consul curl ditaa fdupes ffmpeg figlet git git-filter-repo git-gui
-git-lfs git-subrepo gnu-sed go gomplate govc graphviz imagemagick jq k3sup
-macvim mr nomad packer pandoc putty pwgen pyenv pyenv-virtualenv rename ruby
-rust sshpass terraform tflint tree vault waypoint wget wireguard-tools
+git-lfs git-subrepo gnu-sed go gomplate govc graphviz imagemagick jhead jq
+k3sup macvim mr nomad packer pandoc putty pwgen pyenv pyenv-virtualenv rename
+ruby rust sshpass terraform tflint tree vault waypoint wget wireguard-tools
 '
 
 # brew list --casks | tr ' ' '\n' | sort | tr '\n' ' '
