9cea3e4ad9c04ae56c60fca4049a0d552a532af5
Tyler Tidman <tyler.tidman@draak.ca>
2022-03-05 12:22:52 -0500

    Store info about doing same thing on Arch too

--- bin/dist_upgrade.sh
+++ bin/dist_upgrade.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-# Dist-upgrade
-
 apt-get update
 apt-get --yes dist-upgrade
 apt-get --yes autoremove
 apt-get autoclean
 apt-get clean
+
+# pacman --sync --refresh --noconfirm
+# pacman --sync --sysupgrade --noconfirm
+# pacman --sync --clean --noconfirm
