583502c80eeb71deda9a50e980234ebe9dacfbe7
Tyler Tidman <tyler.tidman@draak.ca>
2022-03-05 12:31:40 -0500

    Add some more notes for combining all the distro upgrade scripts

--- bin/dist_upgrade.sh
+++ bin/dist_upgrade.sh
@@ -9,3 +9,9 @@ apt-get clean
 # pacman --sync --refresh --noconfirm
 # pacman --sync --sysupgrade --noconfirm
 # pacman --sync --clean --noconfirm
+
+# import platform
+# if '2' == platform.python_version()[0]:
+#     print(platform.dist()[0])
+# else:
+#     print(platform.freedesktop_os_release()['ID'])
