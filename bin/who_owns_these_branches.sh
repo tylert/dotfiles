b6ed047aa1b4b887efde341a45c0b7a35ab3ca55
Tyler Tidman <tyler.tidman@draak.ca>
2022-04-01 11:13:57 -0400

    Put some better comments in the helper branch looker script

--- bin/who_owns_these_branches.sh
+++ bin/who_owns_these_branches.sh
@@ -4,17 +4,20 @@
 
 # Don't do anything if we're not inside an existing git repo
 if $(git rev-parse --quiet --git-dir &> /dev/null); then
+    # Dump out current list of remotes and which branch is currently active
     git remote -v
     git branch
 
     echo '-----===[ NOT MERGED ]===-----'
 
+    # Show info for unmerged remote branches
     for branch in $(git branch --remotes --no-merged | grep --invert-match HEAD); do
         echo -e $(git show --format='%h\t%cd\t%cN' --date=format:%Y-%m-%d ${branch} | head -n 1) \\t${branch}
     done | sort -k2
 
     echo '-----===[ MERGED ]===-----'
 
+    # Show info for merged remote branches
     for branch in $(git branch --remotes --merged | grep --invert-match HEAD); do
         echo -e $(git show --format='%h\t%cd\t%cN' --date=format:%Y-%m-%d ${branch} | head -n 1) \\t${branch}
     done | sort -k2
