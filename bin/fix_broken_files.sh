3d276353392d735bd49c0e6f7750d0d3277c23ee
Tyler Tidman <tyler.tidman@draak.ca>
2023-11-16 11:39:01 -0500

    In-progress refactor of the fix_broken_files script

--- bin/fix_broken_files.sh
+++ bin/fix_broken_files.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-# XXX FIXME TODO  Force all files to have Linux/Unix line endings
-# XXX FIXME TODO  Strip trailing whitespace from all lines
-
-# Ensure all files have an EOF delimiter (only process committed files)
+# Process all committed files to apply the desired fixes
 git ls-files -z |\
     while IFS= read -rd '' f; do
+        # Ensure all lines have the appropriate line endings
+        # XXX FIXME TODO
+
+        # Ensure all files have an EOF delimiter at the end
         if file --mime-encoding "${f}" | grep -qv binary; then
             tail -c1 < "${f}" | read -r _ || echo >> "${f}"
         fi
+
+        # Ensure all lines don't have useless space characters at the ends
+        # sed -i 's/[ \t][ \t]*$//' "${f}"
     done
