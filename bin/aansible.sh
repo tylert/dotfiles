3d0a99e7f10cc182ade1506d4b2bfb7dbb207e93
Tyler Tidman <tyler.tidman@draak.ca>
2023-12-08 21:53:07 -0500

    Jettisoning disused ansible wrapper stuff

--- bin/aansible.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Amnesiac ansible
-
-ansible \
-    "${@}" \
-    --ssh-common-args='-o StrictHostKeyChecking=no
-        -o UserKnownHostsFile=/dev/null'
--- bin/aansible_playbook.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Amnesiac ansible-playbook
-
-ansible-playbook \
-    "${@}" \
-    --ssh-common-args='-o StrictHostKeyChecking=no
-        -o UserKnownHostsFile=/dev/null'
