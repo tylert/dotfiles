98d62306678e7992583f225e537b0b8efabcacdc
Tyler Tidman <tyler.tidman@draak.ca>
2025-03-17 11:07:21 -0400

    Optimize the helper script a bit

--- bin/fix_broken_files.sh
+++ bin/fix_broken_files.sh
@@ -7,19 +7,17 @@
 if $(git rev-parse --git-dir &> /dev/null); then
     git ls-files -z |\
         while IFS= read -rd '' f; do
-            # Ensure text files have the appropriate line endings
-            # XXX FIXME TODO
-
-            # Ensure text files don't have useless trailing whitespace on lines
             if file --mime-encoding "${f}" | grep -qv binary; then
+                # Ensure text files have the appropriate line endings
+                # XXX FIXME TODO
+
+                # Ensure text files don't have useless trailing whitespace on lines
                 perl -pi -e 's/[ \t]+$//' "${f}"
                 # XXX FIXME TODO  Make this work on both Linux/Unix and macOS
                 # sed -i '' -E 's/[ '$'\t'']+$//' "${f}"  # BSD sed?
                 # sed -iE 's/[ \t]+$//' "${f}"  # GNU sed?
-            fi
 
-            # Ensure text files have an EOF delimiter at the end
-            if file --mime-encoding "${f}" | grep -qv binary; then
+                # Ensure text files have an EOF delimiter at the end
                 tail -c1 < "${f}" | read -r _ || echo >> "${f}"
             fi
         done
